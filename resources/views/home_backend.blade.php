8b49240e61e73078e3977d6cb4d0a2e099f08030
Balford <externo.hakalab04@consorcio.cl>
2023-02-02 22:30:33 -0600

    validacion de login

--- resources/views/home_backend.blade.php
+++ resources/views/home_backend.blade.php
@@ -4,8 +4,12 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-               
+        @if (Auth::check())
+            <div class="card" style="border-color: black;">
+                <div class="card-header" style="background-color: sandybrown;"><p>Bienvenido, {{ Auth::user()->username }}!</p></div>
+        @else  
         </div>
+        @endif
     </div>
 </div>
 @endsection
